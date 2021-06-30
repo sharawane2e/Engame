92c56b21f0e5badc9c25e14f34580a4d3b97eda7
Amogh-e2e <amogh.chavan@e2eresearch.com>
2021-06-30 20:36:05 +0530

    small changes in scss

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,57 +1,68 @@
-import React,{useState} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import e2eLogo from "../../assets/images/E2E-logo.png";
 import CustomPopup from "../CustomPopup";
 import Login from "../Login";
 import Registration from "../Registration";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 // import clsx from 'clsx';
 
-const Header=(props)=> {
+const Header = (props) => {
   const [isLoginOpen, setLoginIsOpen] = useState(false);
   const [isReginOpen, setReginIsOpen] = useState(false);
-    return (
-      <>
+  return (
+    <>
       <div className="flexGrow">
         <AppBar position="static">
-        <Toolbar className="header-bg">
-          <Typography variant="h6" className="flexGrow">
-            <img src={e2eLogo}/>
-          </Typography>
-          <div className="header-text-color">
-          <Button color="inherit" onClick={() =>setLoginIsOpen(true)}>Login</Button>|
-          <Button color="inherit" onClick={() =>setReginIsOpen(true)}>Registeration</Button>
-        <div className="shoping__card">
-        <ShoppingCartIcon/>
-        </div>
-          </div>
-        </Toolbar>
-      </AppBar>
+          <Toolbar className="header-bg">
+            <Typography variant="h6" className="flexGrow">
+              <img src={e2eLogo} />
+            </Typography>
+            <div className="header-text-color">
+              <Button color="inherit" onClick={() => setLoginIsOpen(true)}>
+                Login
+              </Button>
+              |
+              <Button color="inherit" onClick={() => setReginIsOpen(true)}>
+                Register
+              </Button>
+              <div className="shoping__card">
+                <ShoppingCartIcon fontSize="large" />
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
-      <CustomPopup open={isLoginOpen} onClose={() =>setLoginIsOpen(false)} poupxl={true}> 
+      <CustomPopup
+        open={isLoginOpen}
+        onClose={() => setLoginIsOpen(false)}
+        poupxl={true}
+      >
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={5} className="login-background">
-          </Grid>
+          <Grid item xs={12} sm={5} className="login-background"></Grid>
           <Grid item xs={12} sm={7}>
             <Login data="high" />
           </Grid>
         </Grid>
       </CustomPopup>
-      <CustomPopup open={isReginOpen} onClose={() =>setReginIsOpen(false)}poupxl={true}>
-     <Grid container spacing={3}>
-      <Grid item xs={12} sm={5} className="login-background">
-             </Grid>
-        <Grid item xs={12} sm={7}>
-        <Registration/>
+      <CustomPopup
+        open={isReginOpen}
+        onClose={() => setReginIsOpen(false)}
+        poupxl={true}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={5} className="login-background"></Grid>
+          <Grid item xs={12} sm={7}>
+            <Registration />
           </Grid>
-        </Grid> 
+        </Grid>
       </CustomPopup>
-      </>
-    );
-}
+    </>
+  );
+};
 
 export default Header;
