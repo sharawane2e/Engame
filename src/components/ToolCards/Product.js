d2760feb6e876454383bab7c1bdff0102c9e2d04
Bharat singh <bharat.singh@e2erspl.COM>
2021-07-08 18:33:25 +0530

    add to cart

---
+++ src/components/ToolCards/Product.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+// import Tools from "../../mock/ToolCards";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import ToolDemo from "../ToolDemo";
+import CustomPopup from "../CustomPopup";
+import Button from "@material-ui/core/Button";
+import RemoveRedEyeIcon from '@material-ui/icons/RemoveRedEye';
+import { connect } from 'react-redux';
+import { addToCart } from "../../redux/shopping/shopping-action";
+const Product = ({productData, addToCart}) => {
+    // const [state] = useState(Tools);
+    const [selectedTool, setSelectedTool] = useState(null);
+   
+    const handleToolClick = (tool) => {
+          setSelectedTool(tool);
+    };
+    return (
+        <>
+    <Toolbar className="toolcard">
+              
+                <Paper 
+                  className="toolcard__imageblck"
+                >
+                  <div className="toolcard__image">
+                    <img src={productData.imgUrl} />
+                    <div className="toolcard__preview">
+                     <Button className="toolcard__perview-button"  onClick={() => handleToolClick(productData)}> <RemoveRedEyeIcon className="eyes_icon"/> Preview</Button>
+                    </div>
+                  </div>
+                  <div className="toolcard__align toolcard__toolicons">
+                    <div className="toolcard__items toolcard__download">
+                      {productData.showDownload == false ? (
+                        <span className="toolcard__sub-icons">
+                          <GetAppIcon />
+                        </span>
+                      ) : null}
+                    </div>
+                    <div className="toolcard__items toolcard__shopping">
+                      <span className="toolcard__sub-icons">
+                        <ShoppingCartIcon  onClick={() => addToCart(productData.id)}/>
+                      </span>
+                    </div>
+                  </div>
+                </Paper>
+                <div className="toolcard__align toolcard__toolname">
+                  <div className="toolcard__aligninr1 toolcard__font-family">{productData.toolname}</div>
+                  <div className="toolcard__aligninr toolcard__font-family">${productData.price}</div>
+                </div>
+          
+          
+      <CustomPopup
+        open={selectedTool}
+        onClose={() => setSelectedTool(null)}
+        poupxl={true}
+        headerText=""
+      >
+        <ToolDemo tool={selectedTool}></ToolDemo>
+      </CustomPopup>
+    </Toolbar>
+   </>
+  
+    )
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        addToCart : (id) => dispatch(addToCart(id))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Product)
--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import {HashRouter} from 'react-router-dom';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <HashRouter>
     <App />
    </HashRouter>
+   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
