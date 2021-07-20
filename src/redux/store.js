b92e40e23637c98b475d57eb01d2af452ef17c55
Ashish Kumar <ashish.kumar@e2erspl.COM>
2021-07-20 16:10:14 +0530

    login programming

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import { createStore } from "redux";
+import { createStore  } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+//import rootReducer from "./reducers";
 
 import rootReducer from "./rootReducer";
 
 import { persistStore } from 'redux-persist'
+
+//const middleware = [thunk];
 
 export const store  = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export const persistor = persistStore(store);
 
 export default { store, persistStore };
+
+
+// const store1 = createStore(
+//     rootReducer,
+//     composeWithDevTools(applyMiddleware(...middleware))
+//   );
+  
+//   export default store1;
+
