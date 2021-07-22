0bf19dad5856d693073f57f43e8ef5e201a54428
Bharat singh <bharat.singh@e2erspl.COM>
2021-07-22 19:30:16 +0530

    login

---
+++ src/redux/user/user-action.js
@@ -0,0 +1,23 @@
+import * as actionTypes from './user-types';
+
+export const loginUser = (user) => {
+    return (dispatch) => {
+        try{
+           dispatch({type:actionTypes.LOGIN_SUCCESS, payload:user}) 
+        } catch(error){
+            console.error(error);
+            // dispatch({type:actionTypes.LOGIN_FAIL,payload:{}})
+        }
+    }
+}
+
+export const logOutUser = () => {
+    return (dispatch) => {
+        try{
+           dispatch({type:actionTypes.LOGOUT, payload:{}}) 
+        } catch(error){
+            console.error(error);
+            // dispatch({type:actionTypes.LOGIN_FAIL,payload:{}})
+        }
+    }
+}
