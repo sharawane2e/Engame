f00a30e2e49bb5fc21179cb260d7f8c144ac2039
Bharat singh <bharat.singh@e2erspl.COM>
2021-07-27 13:31:31 +0530

    updates

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,7 +1,6 @@
 import {Redirect} from "react-router-dom";
-import LocalStorageUtils from "./util/LocalStorageUtils";
 import AppRouting from "./AppRouting";
-import { connect, useSelector } from "react-redux";
+import {useSelector } from "react-redux";
 
 const PrivateRoute = (props) =>{
     const Component = props.component;
@@ -12,9 +11,5 @@ const PrivateRoute = (props) =>{
         <Redirect to="/" />
     )
 }
-const mapStateToProps = state => {
-    return {
-        user : state.user.isLoggedIn
-    }
-}
-export default connect(mapStateToProps)(PrivateRoute);
+
+export default PrivateRoute;
