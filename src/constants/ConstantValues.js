4c08e63d27baa44086a73c418eb701ed390ceeab
madhukeshav-e2e <madhukeshav.sharma@e2eresearch.com>
2021-12-08 04:33:29 +0530

    Banner design

--- src/constants/ConstantValues.js
+++ src/constants/ConstantValues.js
@@ -1,4 +1,4 @@
-export const LOGOUT_TIME = 3600000; // Time is in minlisecond
+export const LOGOUT_TIME = 10800000; // Time is in minlisecond
 export const EMAIL_LENGTH = 100;
 export const PASSWORD_MAX_LENGTH = 16;
 export const PASSWORD_MIN_LENGTH = 8;
